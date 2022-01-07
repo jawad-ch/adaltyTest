91ba686bd8ac857edc78235fd1d5dfc7690bee8e
achaari jaouad <jawadaitali636@gmail.com>
2022-01-07 20:14:14 +0100

    (_-_)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
 // elements
-let menu = document.getElementById("menu")
-let menuClosed = document.getElementById("closeMenu")
-let aside = document.getElementById("aside")
+let toggleIcon = document.querySelector(".toggle-icon")
 
 // click events
 
-menu.addEventListener('click', () => {
+toggleIcon.addEventListener('click', () => {
     document.body.classList.toggle('menuOpen')
     document.body.classList.toggle('menuClosed')
-    aside.classList.toggle('open')
-})
-menuClosed.addEventListener('click', () => {
-    document.body.classList.toggle('menuOpen')
-    document.body.classList.toggle('menuClosed')
-        aside.classList.toggle('open')
-
 })
