8a044ae9acb911f94768e568aeb3002bce242711
achaari jaouad <jawadaitali636@gmail.com>
2022-01-07 17:49:14 +0100

    add menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 // elements
 let menu = document.getElementById("menu")
 let menuClosed = document.getElementById("closeMenu")
+let aside = document.getElementById("aside")
 
 // click events
 
 menu.addEventListener('click', () => {
     document.body.classList.toggle('menuOpen')
     document.body.classList.toggle('menuClosed')
+    aside.classList.toggle('open')
 })
 menuClosed.addEventListener('click', () => {
     document.body.classList.toggle('menuOpen')
     document.body.classList.toggle('menuClosed')
+        aside.classList.toggle('open')
+
 })
