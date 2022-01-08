4be550f626ff446513a69e7eefc223cd5c962482
achaari jaouad <jawadaitali636@gmail.com>
2022-01-08 11:49:55 +0100

    responsive 22

--- js/script.js
+++ js/script.js
@@ -6,4 +6,11 @@ let toggleIcon = document.querySelector(".toggle-icon")
 toggleIcon.addEventListener('click', () => {
     document.body.classList.toggle('menuOpen')
     document.body.classList.toggle('menuClosed')
+    // let htmlTag = document.querySelector('html')
+    // if (htmlTag.style.overflow === "hidden") {
+    //     htmlTag.style.overflow = ""
+    // }else {
+    //     htmlTag.style.overflow = "hidden"
+
+    // }
 })
